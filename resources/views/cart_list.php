568d29c2f6f58ed4149862f5ecebfe508a71c260
CreateAnything434 <b7067@oic.jp>
2018-11-19 13:28:09 +0900

    カート画面製作中

--- resources/views/cart_list.php
+++ resources/views/cart_list.php
@@ -1,5 +1,5 @@
 <meta name="viewport" content="width=device-width, initial-scale=1" />
 <link rel="stylesheet" href="/assets/css/main.css" />
 <p>Are you sure?</p>
-商品名：<?=$item->name?>
-価格：<?=$item->price?>
+商品名：<?=$e_items->name?>
+価格：<?=$e_items->price?>
--- resources/views/item_detail.php
+++ resources/views/item_detail.php
@@ -1,2 +1,5 @@
+<meta charset="utf-8" />
+<meta name="viewport" content="width=device-width, initial-scale=1" />
+<link rel="stylesheet" href="/assets/css/main.css" />
 商品名：<?=$e_items->name?>
 価格：<?=$e_items->price?>
