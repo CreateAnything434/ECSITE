d64c026d455514d52cf34f2e0ac259ee30b8c934
CreateAnything434 <b7067@oic.jp>
2019-01-21 15:56:08 +0900

    WebアプリケーションTHE END

--- resources/views/order.php
+++ resources/views/order.php
@@ -11,9 +11,9 @@
   <?=csrf_field()?>
    <div class="box">
     名前：<br><center><input type="text" name="name" placeholder="例)OICはな子" value="<?=$inputs["name"]??''?>" style="width:220px;height:50px"></center>
-    住所：<br><center><input type="text" name="address"   placeholder="例)OIC市OIC町1-23-4" value="<?=$inputs["name"]??''?>" style="width:220px;height:50px"></center>
-    電話番号：<br><center><input type="text" value=""  name="tel" placeholder="例)0004741555" 　style="width:200px;height:50px"></center>
-    Email：<br><center><input type="text" value="" name="email" placeholder="例)hanako@oic.jp" style="width:200px;height:50px"></center>
+    住所：<br><center><input type="text" name="address"   placeholder="例)OIC市OIC町1-23-4" value="<?=$inputs["address"]??''?>" style="width:220px;height:50px"></center>
+    電話番号：<br><center><input type="text"  name="tel" placeholder="例)0004741555"   value="<?=$inputs["tel"]??''?>"style="width:200px;height:50px"></center>
+    Email：<br><center><input type="text" value="<?=$inputs["email"]??''?>" name="email" placeholder="例)hanako@oic.jp" style="width:200px;height:50px"></center>
     </div>
     <br>
     <br>
